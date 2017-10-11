8e04e56060925de97f29e685008de597ec25da4f
Sebastian Andreas Werthemann <werts1@bfh.ch>
2017-10-11 11:36:00 +0200

    startpage init

---
+++ start.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="all" type="text/css" href="cssreset.css"/>
+    <link rel="stylesheet" media="all" type="text/css" href="Style.css"/>
+    <link rel="stylesheet" media="all" type="text/css" href="Responsive.css"/>
+    <link rel="stylesheet" media="all" type="text/css" href="Tester.css"/>
+    <!--<base target="_blank">-->
+    <title>&#10643;Shadowstart&rpargt;</title>
+
+    <style>
+      h1{color:red}
+      p{font.size: 14px}
+    </style>
+
+  </head>
+
+  <body>
+    <div class=row align="center" id=mmr>
+      <div class="col-3" id="mainmenuins"></div>
+      <div class=col-6 align="center" id="mainmenu">
+
+        <a href="http://horriblesubs.info" alt=horriblesubs>
+          <div class="responsive2">
+            <div class="img2" style="background-image: url(images/hs.jpg)">
+            </div>
+          </div>
+        </a>
+
+<!-- row2-->
+        <a href="https://start.duckduckgo.com/" alt=duckduckgo>
+          <div class="responsive">
+            <div class="img" style="background-image: url(images/duckduckgo.png)">
+            </div>
+          </div>
+        </a>
+
+        <a href="http://youtube.com" alt=youtube>
+          <div class="responsive">
+            <div class="img" style="background-image: url(images/youtube.png)">
+            </div>
+          </div>
+        </a>
+
+        <a href="https://moodle.bfh.ch/my" alt=moodle>
+          <div class="responsive">
+            <div class="img" style="background-image: url(images/moodle.png)">
+            </div>
+          </div>
+        </a>
+
+        <a href="http://www.w3schools.com" alt=w3schools>
+          <div class="responsive">
+            <div class="img" style="background-image: url(images/w3schools.png)">
+            </div>
+          </div>
+        </a>
+
+<!-- 5th-->
+        <a href="https://thepiratebay.org/" alt=tpb>
+          <div class="responsive">
+            <div class="img" style="background-image: url(images/tpb.jpg)">
+            </div>
+          </div>
+        </a>
+
+        <a href="https://nyaa.si/" alt=nyaa>
+          <div class="responsive">
+            <div class="img" style="background-image: url(images/nyaa.png)">
+            </div>
+          </div>
+        </a>
+
+        <a href="http://localhost/WebPrograms/WebShop/RamenShop.php" alt=ramenshop>
+          <div class="responsive">
+            <div class="img" style="background-image: url(images/ramen.png)">
+            </div>
+          </div>
+        </a>
+
+        <a href="http://github.com/login/" alt=github>
+          <div class="responsive">
+            <div class="img" style="background-image: url(images/github.png)">
+            </div>
+          </div>
+        </a>
+
+
+      </div>
+    </div>
+
+    <div class=row align="center">
+
+      <!--  Short Links
+
+      <div class="col-2"></div>
+      <div class="col-8"  align="center">
+        <a href="http://localhost/WebPrograms/WebShop/RamenShop.php">RamenShop.php</a><br>
+        <a href="http://github.com/login">github</a><br>
+      </div>
+      </div>
+    -->
+
+    <!--  Scrollpage iframe
+    <iframe style="max-height:100; border:none; -moz-transform: scale(0.6);" width="100%" src="https://www.horriblesubs.info" name="iframe_a"></iframe>
+    -->
+
+    <!--<iframe src="https://duckduckgo.com/search.html?prefill="&bgcolor=#111111&focus=yes" style="overflow:hidden;margin:0;padding:0;width:204px;height:20px,float:right;" frameborder="0"></iframe>
+    -->
+
+
+
+  </body>
+
+</html>
